bfdbd7e521d8f0e1a5524337443463a9e7e2e039
Himanshu-Jain59 <himanshujain29032005@gmail.com>
2025-03-26 15:11:36 +0530

    post ui

---
+++ client/src/components/Posts.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Post from "./Post";
+
+const Posts = () => {
+  return (
+    <div className=" m-5 flex-9/12 flex flex-wrap">
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+    </div>
+  );
+};
+
+export default Posts;
