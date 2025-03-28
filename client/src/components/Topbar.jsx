2222ef2c181e8940c9682be2d791ae331ccd08c3
Himanshu-Jain59 <himanshujain29032005@gmail.com>
2025-03-28 11:14:29 +0530

    topbar ui

---
+++ client/src/components/Topbar.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+const Topbar = () => {
+  return (
+    <div className="bg-white w-full h-[50px] top-0 sticky flex items-center justify-center font-josefin z-10">
+      <div className="flex-3 flex items-center justify-center  gap-3">
+        <i className="text-2xl color-[#444] cursor-pointer fa-brands fa-square-facebook"></i>
+        <i className="text-2xl color-[#444] cursor-pointer fa-brands fa-square-twitter"></i>
+        <i className="text-2xl color-[#444] cursor-pointer fa-brands fa-square-pinterest"></i>
+        <i className="text-2xl color-[#444] cursor-pointer fa-brands fa-square-instagram"></i>
+      </div>
+      <div className="flex-6 flex items-center justify-center">
+        <ul className="flex items-center justify-center gap-5 text-2xl font-light cursor-pointer">
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+          <li>Write</li>
+          <li>Log out</li>
+        </ul>
+      </div>
+
+      <div className="flex-3 flex items-center justify-center gap-5">
+        <img
+          className="h-[40px] w-[40px] rounded-[50%] object-cover"
+          src="https://media.istockphoto.com/id/1495088043/vector/user-profile-icon-avatar-or-person-icon-profile-picture-portrait-symbol-default-portrait.jpg?s=612x612&w=0&k=20&c=dhV2p1JwmloBTOaGAtaA3AW1KSnjsdMt7-U_3EZElZ0="
+        />
+        <i className="text-2xl color-[#666] cursor-pointer fa-solid fa-magnifying-glass"></i>
+      </div>
+    </div>
+  );
+};
+
+export default Topbar;
