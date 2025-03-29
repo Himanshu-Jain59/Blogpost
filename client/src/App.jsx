ef5cbd8e2e9706fa45e2328b1b96698d1eb683b9
Himanshu-Jain59 <himanshujain29032005@gmail.com>
2025-03-29 23:51:41 +0530

    react router dom

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Topbar from "./components/Topbar";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
@@ -9,15 +10,19 @@ import Single from "./pages/Single";
 
 const App = () => {
   return (
-    <>
-      <Topbar />
-      {/* <Home /> */}
-      {/* <Login /> */}
-      {/* <Register /> */}
-      {/* <Single /> */}
-      <Write />
-      {/* <Settings /> */}
-    </>
+    <Router>
+      <>
+        <Topbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/settings" element={<Settings />} />
+          <Route path="/post/:postId" element={<Single />} />
+          <Route path="/write" element={<Write />} />
+        </Routes>
+      </>
+    </Router>
   );
 };
 
