080f6b8e72c99b2524f75a8c8cbd906a00168b7e
Himanshu-Jain59 <himanshujain29032005@gmail.com>
2025-03-30 22:56:25 +0530

    username and email check

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -5,18 +5,33 @@ const bcrypt = require("bcryptjs");
 //Register
 router.post("/register", async (req, res) => {
   try {
-    const salt = await bcrypt.genSalt(10);
-    const hash = await bcrypt.hash(req.body.password, salt);
-    const newUser = new User({
-      username: req.body.username,
-      email: req.body.email,
-      password: hash,
-    });
-    const user = await newUser.save();
-    console.log("user registered");
-    res.status(200).json(user);
+    const { username, email, password } = req.body;
+    if (!username || !email || !password) {
+      return res.status(400).json({ error: "All Fields Are Required!" });
+    }
+    const checkUsers = await User.findOne({ $or: [{ email }, { username }] });
+    if (checkUsers) {
+      return res
+        .status(400)
+        .json({ error: "Username and email already exist" });
+    } else {
+      const salt = await bcrypt.genSalt(10);
+      const hash = await bcrypt.hash(req.body.password, salt);
+      const newUser = new User({
+        username,
+        email,
+        password: hash,
+      });
+      const user = await newUser.save();
+      console.log("user registered");
+      res.status(200).json(user);
+    }
   } catch (err) {
-    console.error(err);
+    // if (err.code == 11000) {
+    //   alert("user already exists");
+    // }
+    // console.log(err);
+    // console.error(err.code);
 
     res.status(500).json(err);
   }
