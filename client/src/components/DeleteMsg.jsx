75383d226852d525cc15a2ab48e2b78dca3ca0b7
Himanshu-Jain59 <himanshujain29032005@gmail.com>
2025-03-31 23:40:15 +0530

    delete confirmation msg box

---
+++ client/src/components/DeleteMsg.jsx
@@ -0,0 +1,45 @@
+import React, { useContext } from "react";
+import { Context } from "../context/Context";
+import axios from "axios";
+
+const DeleteMsg = ({ onCancel }) => {
+  const { user, dispatch } = useContext(Context);
+  const handleDelete = async () => {
+    try {
+      await axios.delete(`/api/user/${user._id}`, {
+        data: { userId: user._id },
+      });
+      dispatch({ type: "LOGOUT" });
+      window.location.replace("/");
+    } catch (error) {
+      console.error(err);
+    }
+  };
+  return (
+    <div className=" absolute  z-10 flex  flex-col justify-center items-center w-screen h-[92vh] backdrop-brightness-50 ">
+      <div className="bg-white flex  flex-col justify-center items-center w-90 h-80 border-3 border-gray-300 rounded-xl">
+        <i className=" m-4 fa-regular fa-circle-xmark text-6xl text-[#ec3c3c]"></i>
+
+        <p className="mb-2 font-semibold text-2xl">Are you sure?</p>
+        <p>You won't be able to revert this.</p>
+        <div>
+          <button
+            className="px-4 py-1 border-2 font-semibold text-gray-600 border-gray-400 cursor-pointer rounded-lg m-6"
+            onClick={onCancel}
+          >
+            Cancel
+          </button>
+
+          <button
+            className="px-4 py-1 border-2 font-semibold  border-red-700 text-white cursor-pointer rounded-lg m-6 bg-red-500"
+            onClick={handleDelete}
+          >
+            Delete
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DeleteMsg;
