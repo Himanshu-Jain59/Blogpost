86c06faadf96dc4b3affe5f8ef20f830ab1aa6f1
Himanshu-Jain59 <himanshujain29032005@gmail.com>
2025-04-01 23:28:12 +0530

    toastify

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,12 +1,26 @@
 import axios from "axios";
-import React, { useContext, useRef } from "react";
+import React, { useContext, useRef, useState } from "react";
 import { Link } from "react-router-dom";
 import { Context } from "../context/Context";
+import { toast } from "react-toastify";
 
 const Login = () => {
   const userRef = useRef();
   const passwordRef = useRef();
+  const notify = () =>
+    toast.error("Invalid Credentials", {
+      position: "top-center",
+      autoClose: 2000,
+      theme: "colored",
+    });
+  const successfull = () =>
+    toast.success("Login Successfull", {
+      position: "top-center",
+      autoClose: 1500,
+      theme: "colored",
+    });
   const { user, dispatch } = useContext(Context);
+
   const handleSubmit = async (e) => {
     e.preventDefault();
     dispatch({ type: "LOGIN_START" });
@@ -15,13 +29,19 @@ const Login = () => {
         username: userRef.current.value,
         password: passwordRef.current.value,
       });
+      successfull();
+      // console.log(res);
 
       dispatch({ type: "LOGIN_SUCCESS", payload: res.data });
     } catch (err) {
+      if (err.response.data) {
+        notify();
+      }
+      // console.error(err);
       dispatch({ type: "LOGIN_FAILURE" });
     }
   };
-  console.log(user);
+  // console.log(user);
   return (
     <div className="bg-[url('https://a-z-animals.com/media/2022/10/iStock-1298291139-1024x680.jpg')] bg-cover bg-no-repeat font-lora">
       <div className="  backdrop-blur-xs  w-full h-[92vh] flex flex-col justify-center items-center  ">
@@ -37,15 +57,17 @@ const Login = () => {
             />
             <label className="my-2 text-2xl ml-2 ">Password</label>
             <input
-              className="p-2 px-4 bg-white rounded-md outline-0 "
+              className="p-2 px-4 bg-white rounded-md outline-0  "
               type="password"
               placeholder="Enter password"
               ref={passwordRef}
             />
+
             <button className="mt-5 p-3 bg-blue-400 rounded-full text-white font-semibold text-xl tracking-wider cursor-pointer">
               Login
             </button>
           </form>
+
           <div>
             <span className="text-lg text-white">Don't have an account?</span>
 
