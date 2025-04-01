d47795cfe0f9ebaedb84b38f0f6407da09c2ac50
Himanshu-Jain59 <himanshujain29032005@gmail.com>
2025-04-01 23:26:21 +0530

    using quotes api

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,6 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const Header = () => {
+  const [quote, setQuote] = useState("");
+  const [author, setAuthor] = useState("");
+  const options = {
+    method: "GET",
+    url: "https://quotes-api12.p.rapidapi.com/quotes/random",
+    params: { type: "happiness" },
+    headers: {
+      "x-rapidapi-key": "8b3ce5f100mshf1ba64f85626f53p1e6dcejsn57b838eaf172",
+      "x-rapidapi-host": "quotes-api12.p.rapidapi.com",
+    },
+  };
+  useEffect(() => {
+    return async () => {
+      try {
+        const response = await axios.request(options);
+        setQuote(response.data.quote);
+        setAuthor(response.data.author);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+  }, []);
+
   return (
     <>
       <div className="mt-15">
@@ -13,6 +37,17 @@ const Header = () => {
           src="https://royalcitynursery.com/wp-content/uploads/elementor/thumbs/royal-city-nursery-the-importance-of-sun-exposure-sunlight-qb41eglqn3f0vlk7z1lbtmprbj1fmi5i2j3t5lzafc.jpg "
           alt=""
         />
+
+        {quote && (
+          <span className="m-2 font-lora font-semibold text-4xl max-w-[1350px]  w-max absolute top-[60%] left-[50%] transform -translate-x-1/2 -translate-y-1/2 ">
+            " {quote} "
+          </span>
+        )}
+        {author && (
+          <span className=" font-josefin font-semibold text-4xl absolute top-[75%] left-[50%] transform -translate-x-1/2 -translate-y-1/2 text-white">
+            -{author}
+          </span>
+        )}
       </div>
     </>
   );
