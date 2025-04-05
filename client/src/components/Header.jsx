219a76f464116bee7acb6d4e1ef9b79ef0f4c0ff
Himanshu-Jain59 <himanshujain29032005@gmail.com>
2025-04-05 17:19:18 +0530

    footer added

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -30,7 +30,7 @@ const Header = () => {
       <div className="mt-15">
         <div className="flex flex-col items-center font-lora color-[#444] italic">
           <span className="absolute top-[12%] text-2xl">React & Node</span>
-          <span className="absolute top-[13%] text-[125px]">Blogpost</span>
+          <span className="absolute top-[13%] text-[125px] ">Blogpost</span>
         </div>
         <img
           className=" w-full h-120  object-cover mt-16"
--- client/src/pages/Single.jsx
+++ client/src/pages/Single.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Sidebar from "../components/Sidebar";
 import Singlepost from "../components/Singlepost";
+import Footer from "../components/Footer";
 
 const Single = () => {
   return (
     <>
       <Singlepost />
       {/* <Sidebar /> */}
+      <Footer />
     </>
   );
 };
