50780a3bec831a44ac09dd328e936e79fa6fab9b
Himanshu-Jain59 <himanshujain29032005@gmail.com>
2025-04-10 21:44:48 +0530

    using prime react ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useContext } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
+import { PrimeReactProvider } from "primereact/api";
 import Topbar from "./components/Topbar";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
@@ -8,26 +9,38 @@ import Register from "./pages/Register";
 import Write from "./pages/Write";
 import Settings from "./pages/Settings";
 import Single from "./pages/Single";
+import Contact from "./pages/Contact";
 import { Context } from "./context/Context";
+import About from "./pages/About";
 
 const App = () => {
   const { user } = useContext(Context);
   return (
     <>
-      <Router>
-        <>
-          <Topbar />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/register" element={user ? <Home /> : <Register />} />
-            <Route path="/login" element={user ? <Home /> : <Login />} />
-            <Route path="/settings" element={user ? <Settings /> : <Login />} />
-            <Route path="/post/:postId" element={<Single />} />
-            <Route path="/write" element={user ? <Write /> : <Login />} />
-          </Routes>
-        </>
-      </Router>
-      <ToastContainer />
+      <PrimeReactProvider>
+        <Router>
+          <>
+            <Topbar />
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/contact" element={<Contact />} />
+              <Route
+                path="/register"
+                element={user ? <Home /> : <Register />}
+              />
+              <Route path="/login" element={user ? <Home /> : <Login />} />
+              <Route
+                path="/settings"
+                element={user ? <Settings /> : <Login />}
+              />
+              <Route path="/post/:postId" element={<Single />} />
+              <Route path="/write" element={user ? <Write /> : <Login />} />
+            </Routes>
+          </>
+        </Router>
+        <ToastContainer />
+      </PrimeReactProvider>
     </>
   );
 };
