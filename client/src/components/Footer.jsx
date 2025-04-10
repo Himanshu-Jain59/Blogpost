6e8b9094e4e54e5d4db351f1b8494e12d3144fba
Himanshu-Jain59 <himanshujain29032005@gmail.com>
2025-04-10 21:45:52 +0530

    color prop

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,24 +1,34 @@
 import React from "react";
 
-const Footer = () => {
+const Footer = ({ color }) => {
   return (
-    <div className=" flex items-center justify-between h-[200px] bg-amber-400 p-2 px-15">
+    <div
+      className={` flex items-center justify-between h-[200px] ${color} p-2 px-15`}
+    >
       <div className="flex flex-col gap-15 ">
         <span className="font-santaLove text-6xl ">blogpost</span>
         <span className="font-varela text-sm ">
           &copy; 2025 Blogpost | Inspire, Connect, Share
         </span>
       </div>
-      <div className="flex items-center justify-around w-1/2">
+      <div className="flex mt-3 justify-around w-1/2">
         <div className="">
           <span className=" p-2  mt-2 font-varela text-xl/[20px] color-[#222] font-semibold text-center ">
             Company
           </span>
           <ul className="mt-1 p-2 font-varela text-base color-[#222] cursor-pointer  ">
             <li className="hover:underline">About us</li>
-            <li className="hover:underline">Team</li>
             <li className="hover:underline">Contact</li>
             <li className="hover:underline">Help</li>
+          </ul>
+        </div>
+        <div className="">
+          <span className=" p-2  mt-2 font-varela text-xl/[20px] color-[#222] font-semibold text-center ">
+            Community
+          </span>
+          <ul className="mt-1 p-2 font-varela text-base color-[#222] cursor-pointer  ">
+            <li className="hover:underline">Discord</li>
+            <li className="hover:underline">Twitch</li>
           </ul>
         </div>
         <div className=" ">
@@ -29,14 +39,7 @@ const Footer = () => {
             <li className="hover:underline">Facebook</li>
             <li className="hover:underline">Instagram</li>
             <li className="hover:underline">Twitter</li>
-            <li className="hover:underline">LinkedIn</li>
           </ul>
-          {/* <div className=" flex items-center justify-center  gap-3 w-[250px] mt-2">
-            <i className="text-2xl color-[#444] cursor-pointer fa-brands fa-square-facebook"></i>
-            <i className="text-2xl color-[#444] cursor-pointer fa-brands fa-square-twitter"></i>
-            <i className="text-2xl color-[#444] cursor-pointer fa-brands fa-square-instagram"></i>
-            <i className="text-2xl color-[#444] cursor-pointer fa-brands fa-square-x-twitter"></i>
-          </div> */}
         </div>
         <div className=" ">
           <span className=" p-2  font-varela text-xl/[20px] color-[#222] font-semibold text-center ">
