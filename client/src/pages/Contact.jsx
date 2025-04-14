390dcdf273cb440bbfc5824871dcaf359231bfc8
Himanshu-Jain59 <himanshujain29032005@gmail.com>
2025-04-14 21:51:32 +0530

    paths modified

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -4,15 +4,16 @@ import Footer from "../components/Footer";
 const Contact = () => {
   return (
     <>
-      <div className=" mt-5 mx-10 bg-[url('https://img.freepik.com/premium-photo/top-view-office-desk-workspace_160097-448.jpg?w=996')] rounded-3xl bg-[#504e4e]  h-130 bg-cover bg-no-repeat flex flex-col justify-center items-center">
-        {/* <div className=" mt-5 bg-[url('https://img.freepik.com/premium-photo/top-view-office-desk-workspace_160097-448.jpg?w=996')] w-full h-140 bg-cover bg-no-repeat flex flex-col justify-center items-center"> */}
-        <span className=" text-8xl text-white font-lora ">Get in touch</span>
-        <br />
-        <span className="text-xl w-1/2 text-white font-lora text-center">
-          Whether you have a question, feedback, or just want to say hello —
-          we’re all ears! Reach out to us using the form below or through our
-          social channels.
-        </span>
+      <div className=" mt-5 mx-10 bg-[url('https://img.freepik.com/premium-photo/top-view-office-desk-workspace_160097-448.jpg?w=996')] rounded-3xl bg-[#504e4e]  h-130 bg-cover bg-no-repeat ">
+        <div className="h-full  rounded-3xl backdrop-brightness-70 flex flex-col justify-center items-center ">
+          <span className=" text-8xl text-white font-lora ">Get in touch</span>
+          <br />
+          <span className="text-xl w-1/2 text-white font-lora text-center">
+            Whether you have a question, feedback, or just want to say hello —
+            we’re all ears! Reach out to us using the form below or through our
+            social channels.
+          </span>
+        </div>
       </div>
 
       {/* src="https://cdn.pixabay.com/photo/2023/09/29/14/58/road-8284023_960_720.jpg" */}
@@ -55,18 +56,18 @@ const Contact = () => {
         </div>
       </div>
       {/* bg-[#42676f]  */}
-      <div className=" m-10 rounded-3xl flex flex-col justify-center items-center gap-4 p-20 bg-[url('/forest.jpeg')]  bg-cover bg-no-repeat  ">
+      <div className=" m-10 rounded-3xl flex flex-col justify-center items-center gap-4 p-20 bg-[url('/images/forest.jpeg')]  bg-cover bg-no-repeat  ">
         <span className="text-5xl text-white font-varela text-center">
           Your next favorite blog is just a click away.
         </span>
         <span className="text-xl text-white font-varela text-center">
           Start your journey with Blogpost.
         </span>
-        <button className="text-white bg-[#42676f] text-lg font-semibold font-varela p-3 w-40 rounded-full cursor-pointer">
+        <button className="text-white bg-[#619471] text-lg font-semibold font-varela p-3 w-40 rounded-full cursor-pointer">
           Get Started
         </button>
       </div>
-      <Footer color={"bg-[#42676f] text-white"} />
+      <Footer color={"bg-[#619471] text-white"} />
     </>
   );
 };
